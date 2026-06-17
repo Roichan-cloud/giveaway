6c617ff2b61cfe4fe1a4c1e9e11b3c302259ded1
Frre-Cloud <roichannoob@gmail.com>
2026-06-18 05:28:09 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// ================================================================
+// 🔥 FIREBASE CONFIG
+// ================================================================
+
+const firebaseConfig = {
+  apiKey:            "AIzaSyA8hka0fQmyKbKdmbra8yuS8xsGClVdYR0",
+  authDomain:        "giveaway-photonic-lighting.firebaseapp.com",
+  projectId:         "giveaway-photonic-lighting",
+  storageBucket:     "giveaway-photonic-lighting.firebasestorage.app",
+  messagingSenderId: "479998026581",
+  appId:             "1:479998026581:web:b6dc7883d5c3a478c105dc"
+};
+
+export { firebaseConfig };
